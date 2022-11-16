c00dd54c08a368b619e2099e9f85c695279c8026
Romain FRANCOIS <romain.francois3@hotmail.fr>
2022-11-16 12:41:32 +0100

    1.0.25

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -8,15 +8,25 @@ use Symfony\Component\HttpFoundation\Request;
 
 abstract class Controller extends AbstractController {
 
-    protected Request $request;
+    private Request $request;
 
-    protected function checkFormIsValid(FormInterface $form){
+    protected function checkFormIsValid(FormInterface $form):bool{
         return $form->isSubmitted() && $form->isValid();
     }
 
-    protected function createForm(string $type, $data = null, array $options = []): FormInterface{
+    protected function createForm(string $type, $data = null, array $options = ['method' => 'POST']): FormInterface{
         $form = parent::createForm($type, $data, $options);
         $form->handleRequest($this->request);
         return $form;
     }
+
+    protected function setRequest(Request $request){
+        $this->request = $request;
+    }
+
+    public function getRequest():Request{
+        return $this->request;
+    }
+
+
 }

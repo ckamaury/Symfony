b0c86653105f57a178521ee7562cc4f07363d868
Romain FRANCOIS <romain.francois3@hotmail.fr>
2023-01-27 14:37:51 +0100

    1.0.57

--- src/Master/Action.php
+++ src/Master/Action.php
@@ -15,19 +15,29 @@ abstract class Action {
     abstract protected function getAccess();
     abstract protected function createAccess();
 
+    public function setIsSuccess(bool $isSuccess): self {
+        $this->isSuccess = $isSuccess;
+        return $this;
+    }
+
     protected function addOneMessage(string $message):void{
         $this->messages[] = $message;
     }
-    protected function flush():bool{
-        if($this->isDevEnv()) Database::flush();
-        else{
-            try{Database::flush();}
-            catch(\ErrorException $e){
-                $this->addOneMessage('Erreur lors de la sauvegarde en base de données.');
-                return FALSE;
-            }
+    protected function rejectWithMessage(string $message):void{
+        $this->setIsSuccess(FALSE);
+        $this->addOneMessage($message);
+    }
+    protected function flush():void{
+        try{
+            Database::flush();
         }
-        return TRUE;
+        catch(\Error|\Exception $e){
+            $this->rejectWithError($e);
+        }
+    }
+    protected function rejectWithError(\Error|\Exception $error):void{
+        $this->rejectWithMessage("Erreur logicielle, merci de contacter l'administrateur.");
+        if($this->isDevEnv()) throw new \ErrorException($error->getMessage());
     }
     protected function isDevEnv():bool{
         return APP::getKernel()->getEnvironment() === 'dev';

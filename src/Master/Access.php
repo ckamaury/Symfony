a84d326688ee495191d633a241c9e408d160f436
Romain FRANCOIS <romain.francois3@hotmail.fr>
2023-01-27 13:53:50 +0100

    1.0.52

--- src/Master/Access.php
+++ src/Master/Access.php
@@ -6,8 +6,13 @@ abstract class Access {
 
     private bool $hasAccess = FALSE;
 
+    private array $messages = [];
+
     protected function authorizedAccess(): void {
         $this->hasAccess = TRUE;
+    }
+    protected function addOneMessage(string $message):void{
+        $this->messages[] = $message;
     }
 
     public function granted(): bool {
@@ -16,5 +21,9 @@ abstract class Access {
     public function rejected(): bool {
         return !$this->granted();
     }
+    public function getMessages(): array {
+        return $this->messages;
+    }
+
 
 }
--- src/Master/Action.php
+++ src/Master/Action.php
@@ -2,14 +2,52 @@
 
 namespace CkAmaury\Symfony\Master;
 
+use CkAmaury\Symfony\APP;
+use CkAmaury\Symfony\Database\Database;
+
 abstract class Action {
 
     protected Access $access;
 
-    public function getAccess(): Access {
+    private bool $isSuccess = FALSE;
+    private array $messages = [];
+
+    protected function getAccess(){
         return $this->access;
     }
 
+    protected function addOneMessage(string $message):void{
+        $this->messages[] = $message;
+    }
+    protected function flush():bool{
+        if($this->isDevEnv()) Database::flush();
+        else{
+            try{Database::flush();}
+            catch(\ErrorException $e){
+                $this->addOneMessage('Erreur lors de la sauvegarde en base de données.');
+                return FALSE;
+            }
+        }
+        return TRUE;
+    }
+    protected function isDevEnv():bool{
+        return APP::getKernel()->getEnvironment() === 'dev';
+    }
+    protected function isProdEnv():bool{
+        return APP::getKernel()->getEnvironment() === 'prod';
+    }
 
+    public function getMessages(): array {
+        return array_merge($this->getAccess()->getMessages(),$this->messages) ;
+    }
+    private function isSuccess():bool{
+        return $this->isSuccess;
+    }
+    public function succeeded():bool{
+        return $this->getAccess()->granted() && $this->isSuccess();
+    }
+    public function failure():bool{
+        return !$this->succeeded();
+    }
 
 }

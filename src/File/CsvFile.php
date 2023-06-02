5263407f1635ff8d80a4faa7064f2e37b3f997ff
Romain FRANCOIS <romain.francois3@hotmail.fr>
2023-06-02 11:15:07 +0200

    ADD File

---
+++ src/File/CsvFile.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace CkAmaury\Symfony\File;
+
+class CsvFile extends File {
+
+    protected int $row_current_number = 0;
+    protected string $delimiter = ';';
+
+    public function setDelimiter(string $delimiter): self{
+        $this->delimiter = $delimiter;
+        return $this;
+    }
+
+    //####### WRITER #######
+    public function putDataWithHeaders(array $data){
+        array_unshift($data,$this->extractHeaders($data));
+        $this->putData($data);
+
+    }
+    public function putData(array $data){
+        $this->openWriter();
+        foreach ($data as $row) {
+            $this->putRow($row);
+        }
+        $this->closeWriter();
+    }
+
+    private function putRow(array $row){
+        fputcsv($this->fileputter,$row,$this->delimiter);
+    }
+    private function extractHeaders(array $data) : array{
+        $first_row = $data[array_key_first($data)];
+        return array_keys($first_row);
+    }
+
+    //####### READER #######
+    public function openReader(){
+        parent::openReader();
+        $this->row_current_number = 0;
+    }
+    public function getData(?string $delimiter = null) : array{
+        if(!is_null($delimiter)){
+            $this->setDelimiter($delimiter);
+        }
+        $this->openReader();
+        $data = array();
+        while( ($row = $this->getNextRow()) !== FALSE ){
+            $data[] = $row;
+        }
+        return $data;
+    }
+    public function getNextRow(){
+        $row = fgetcsv($this->handle, 0, $this->delimiter);
+        if($row !== FALSE){
+            $this->sanitizeRow($row);
+            $this->row_current_number++;
+        }
+        else{
+            $this->closeReader();
+        }
+        return $row;
+    }
+    public function getRow(int $rowNumber){
+        if($rowNumber < 1){
+            Throw New \Exception("Row Number must be >= 1");
+        }
+
+        if($this->row_current_number >= $rowNumber){
+            $this->openReader();
+        }
+
+        do{
+            $row = $this->getNextRow();
+        }while($this->row_current_number != $rowNumber && $row !== FALSE);
+
+        return $row;
+    }
+
+    private function sanitizeRow(array &$row){
+        foreach($row as &$value){
+            $value = trim($value);
+            $value = ($value != '') ? $value : null;
+        }
+    }
+}
---
+++ src/File/File.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace CkAmaury\Symfony\File;
+
+class File {
+
+    protected string $path;
+
+    protected $fileputter = null;
+
+    /**
+     * @var resource|false|null
+     */
+    protected $handle = null;
+
+    private CONST CHMOD_FOLDER = 0755;
+    private CONST CHMOD_FILE = 0770;
+
+    public function __construct(?string $path = null){
+        if(!is_null($path)){
+            $this->path = $path;
+        }
+    }
+    public function __destruct(){
+        $this->closeReader();
+    }
+
+    public function setPath($path) : self{
+        $this->path = $path;
+        return $this;
+    }
+    public function getPath(): string {
+        return $this->path;
+    }
+    public function getName(): string {
+        return basename($this->path);
+    }
+
+    public function isExisting() : bool{
+        return file_exists($this->path);
+    }
+    public function isReadable() : bool{
+        return is_readable($this->path);
+    }
+
+    public function create(){
+        if(!$this->isExisting()){
+            $this->checkAndCreateFolder();
+            file_put_contents($this->path,'');
+            chmod($this->path,self::CHMOD_FILE);
+        }
+    }
+    public function rename(string $newPath){
+        $oldPath = $this->path;
+        $this->changePath($newPath);
+        rename($oldPath,$this->path);
+    }
+    public function copy(string $newPath) : File{
+        $new_file = new File();
+        $new_file
+            ->setPath($newPath)
+            ->create();
+        copy($this->path,$newPath);
+        return $new_file;
+    }
+    public function delete(){
+        unlink($this->path);
+    }
+
+    private function checkAndCreateFolder(){
+        $level = $this->searchLevelOfFolderExist();
+        $this->createFoldersInCascade($level);
+    }
+    private function searchLevelOfFolderExist() : int{
+        $level = 0;
+        do{
+            $level++;
+            $dirname = dirname($this->path,$level);
+        }while(!is_dir($dirname));
+        return $level;
+    }
+    private function createFoldersInCascade($level){
+        for($level--;$level > 0;$level--){
+            $dirname = dirname($this->path,$level);
+            mkdir($dirname,self::CHMOD_FOLDER);
+        }
+    }
+
+    private function changePath(string $newPath){
+        $this->setPath($newPath);
+        $this->checkAndCreateFolder();
+    }
+
+    //####### WRITER #######
+    public function openWriter(){
+        $this->fileputter = fopen($this->getPath(), 'w');
+    }
+    public function closeWriter(){
+        if(!is_null($this->fileputter)){
+            fclose($this->fileputter);
+            $this->fileputter = null;
+        }
+    }
+    public function putContents($text,int $flags = 0):bool{
+        return (file_put_contents($this->path, $text, $flags) !== FALSE);
+    }
+    public function download(string $url,?bool $forceUtf8 = false){
+        $contextOptions = array(
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false,
+            ),
+        );
+        $contents = file_get_contents(
+            $url,
+            false,
+            stream_context_create($contextOptions)
+        );
+        if($forceUtf8){
+            $contents = utf8_encode($contents);
+        }
+
+        if(!$this->putContents($contents)) {
+            throw new \Exception('Downloading is unsuccessful.');
+        }
+    }
+
+    //####### READER #######
+    public function openReader(){
+        $this->closeReader();
+        $this->handle = fopen($this->path, "r");
+    }
+    public function closeReader(){
+        if(!is_null($this->handle)){
+            fclose($this->handle);
+            $this->handle = null;
+        }
+    }
+    public function getContents(){
+        return file_get_contents($this->path, true);
+    }
+    public function getData() : array{
+        return array();
+    }
+    public function getDataWithHeaders() : array{
+        $data = $this->getData();
+        $this->replaceKeysByFirstLine($data);
+        return $data;
+    }
+
+    //####### GLOBAL FUNCTIONS #######
+    protected function replaceKeysByFirstLine(array &$array){
+        $headers = array_shift($array);
+        foreach($array as &$row){
+            $this->replaceKeys($row,$headers);
+        }
+    }
+    protected function replaceKeys(array &$row,array $headers){
+        foreach($row as $old_key => $value){
+            $new_key = $headers[$old_key];
+            $row[$new_key] = $value;
+            unset($row[$old_key]);
+        }
+    }
+}

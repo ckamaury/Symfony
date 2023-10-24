71e8d39a4976602e9d65cddd498e51a938510b14
Romain FRANÇOIS <romain.francois@dev-fighters.fr>
2023-10-24 07:58:50 +0200

    ADD functions in Console

--- src/Console/Console.php
+++ src/Console/Console.php
@@ -3,6 +3,7 @@
 namespace CkAmaury\Symfony\Console;
 
 use CkAmaury\Symfony\APP;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Console {
@@ -12,6 +13,20 @@ class Console {
     }
     private static function getOutputConsole():?OutputInterface{
         return APP::getOutputConsole();
+    }
+
+
+    public static function exitWithError(string $message):int{
+        self::write('<error>                                          </error>');
+        self::write($message);
+        self::write('<error>                                          </error>');
+        return Command::FAILURE;
+    }
+    public static function exitWithSuccess(string $message):int{
+        self::write('<bg=green>                                          </>');
+        self::write($message);
+        self::write('<bg=green>                                          </>');
+        return Command::SUCCESS;
     }
 
 }
